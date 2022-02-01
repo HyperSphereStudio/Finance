ec772d45e80370de7e699775f67bc4ddf576a626
Johnathan Bizzano <johnathan.bizzano@gmail.com>
2022-02-01 13:37:24 -0500

    NA

---
+++ src/Finance.jl
@@ -0,0 +1,27 @@
+module Finance
+	using HTTP
+	using JSON
+
+	export YahooQuote, ParseInternet, YahooOptionChain
+
+	ParseInternet(url) = JSON.parse(String(HTTP.get(url).body))
+
+	function YahooQuote(sym)
+		res = ParseInternet("https://query1.finance.yahoo.com/v7/finance/quote?symbols=$sym")
+		if haskey(res, "quoteResponse")
+			return res["quoteResponse"]["result"][1]
+		end
+		res
+	end
+
+	function YahooOptionChain(sym)
+		res = ParseInternet("https://query1.finance.yahoo.com/v7/finance/options/$sym")
+		if haskey(res, "optionChain")
+			return res["optionChain"]["result"][1]["options"][1]
+		end
+		res
+	end
+end
+
+
+
