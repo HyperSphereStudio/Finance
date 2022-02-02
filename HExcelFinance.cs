0689b56fc57127fb0492eb0db264b81065bf77f7
Johnathan Bizzano <johnathan.bizzano@gmail.com>
2022-02-02 09:25:55 -0500

    Version 0.0

---
+++ HExcelFinance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JuliaInterface;
+using ExcelDna.Integration;
+using HyperSphere;
+
+public class HExcelFinance
+{
+    private static JLFun parseInternet, yahooQuote;
+    private static JLType yahooOptionChain;
+
+
+    [ExcelFunction(IsThreadSafe = true, IsMacroType = true)] public static object ParseInternet(string url) => Nt.Box(parseInternet.Invoke(url));
+    [ExcelFunction(IsThreadSafe = true, IsMacroType = true)] public static object YahooQuote(string symbol) => Nt.Box(yahooQuote.Invoke(symbol));
+    [ExcelFunction(IsThreadSafe = true, IsMacroType = true)] public static object YahooOptionChain(string symbol) => Nt.Box(yahooOptionChain.Create(symbol));
+
+    public static string[] Main() {
+        parseInternet = Julia.Eval("ParseInternet");
+        yahooQuote = Julia.Eval("YahooQuote");
+        yahooOptionChain = Julia.Eval("YahooOptionChain");
+
+        return new[] { "ParseInternet", "YahooQuote" , "YahooOptionChain"};
+    }
+
+
+}
