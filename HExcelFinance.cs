f70e3bee1fe3edd98bc389dda7a80deebf6a72a1
Johnathan Bizzano <johnathan.bizzano@gmail.com>
2022-02-03 00:04:53 -0500

    Version 2.0

--- HExcelFinance.cs
+++ HExcelFinance.cs
@@ -9,17 +9,28 @@ using HyperSphere;
 
 public class HExcelFinance
 {
-    private static JLFun parseInternet, yahooQuote, yahooOptionChain;
+    private static JLFun parseInternet, yahooQuote, yahooOptionChain, serializeFile, deserializeFile;
 
 
     [ExcelFunction(Name = "Finance.ParseInternet", IsThreadSafe = true, IsMacroType = true)] public static object ParseInternet(string url) => Nt.Box(parseInternet.Invoke(url));
     [ExcelFunction(Name = "Finance.YahooQuote", IsThreadSafe = true, IsMacroType = true)] public static object YahooQuote(string symbol) => Nt.Box(yahooQuote.Invoke(symbol));
     [ExcelFunction(Name = "Finance.YahooOptionChain", IsThreadSafe = true, IsMacroType = true)] public static object YahooOptionChain(string symbol) => Nt.Box(yahooOptionChain.Invoke(symbol));
+    
+    [ExcelFunction(Name = "Finance.SerializeFile", IsThreadSafe = true)] public static object SerializeFile(string dir, string fileName, object o)
+    {
+        serializeFile.Invoke(dir, fileName, Jl.Xlunbox(o));
+        return "Serialized File!";
+    }
+
+    [ExcelFunction(Name = "Finance.DeserializeFile", IsThreadSafe = true)] public static object DeserializeFile(string dir, string fileName) => Nt.Box(deserializeFile.Invoke(dir, fileName));
+
 
     public static Type[] Main() {
         parseInternet = Julia.Eval("ParseInternet");
         yahooQuote = Julia.Eval("YahooQuote");
         yahooOptionChain = Julia.Eval("YahooOptionChain");
+        serializeFile = Julia.Eval("SerializeFile");
+        deserializeFile = Julia.Eval("DeserializeFile");
 
         return new Type[0];
     }
